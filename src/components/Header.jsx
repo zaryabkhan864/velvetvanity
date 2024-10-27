3908179c76a9c28ba7d1879c9f68850e3b138867
Aysan Ebrahimi <aysanebrahimi1998@gmail.com>
2024-10-27 18:06:31 +0330

    the style of Header.jsx is changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 
 function Header() {
-  return <header>
-    <nav>
-    <a href="#new-arrivals">New Arrivals</a>
-    <a href="#bestsellers">Bestsellers</a>
-    <a href="#skincare">Skincare</a>
-    <a href="#makeup">Makeup</a>
-    <a href="#hair">Hair</a>
-    <a href="#accessories">Accessories</a>
-    <a href="#blog">Blog</a>
-    <a href="#sale">Sale</a>
+  return <header className="p-4 bg-white shadow-md ">
+    <nav className="flex justify-center space-x-4 text-pink-300">
+    <a className="hover:text-pink-500" href="#new-arrivals">New Arrivals</a>
+    <a className="hover:text-pink-500" href="#bestsellers">Bestsellers</a>
+    <a className="hover:text-pink-500" href="#skincare">Skincare</a>
+    <a className="hover:text-pink-500" href="#makeup">Makeup</a>
+    <a className="hover:text-pink-500" href="#hair">Hair</a>
+    <a className="hover:text-pink-500" href="#accessories">Accessories</a>
+    <a className="hover:text-pink-500" href="#blog">Blog</a>
+    <a className="hover:text-pink-500" href="#sale">Sale</a>
     </nav>
   </header>;
 }
