e76a5521891e406832c183221ad1ae001385e85b
Aysan Ebrahimi <aysanebrahimi1998@gmail.com>
2024-10-31 13:13:59 +0330

    images were set and new styles were applied to the categories &products & header components

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,17 +1,20 @@
 function Categories() {
   const categories = [
-    { name: "New arrivals", image: "../constants/newarrival.png" },
-    { name: "skincare", image: "../constants/skincare.png" },
-    { name: "Makeup", image: "../constants/makeup.png" },
+    { name: "NEW ARRIVALS", image: require("../constants/newarrival.png") },
+    { name: "SKINCARE", image: require("../constants/skincare.png") },
+    { name: "MAKEUP", image: require("../constants/makeup.png") },
   ];
 
   return (
     <section className="py-1- bg-white">
-      <div className="flex justify-center space-x-8">
+      <div className="flex justify-center space-x-8 pb-4">
         {categories.map((category, index) => (
-          <div key={index} className="text-center">
-            <img src={category.image} alt={category.name} />
-            <h3 className="mt-4 text-lg text-gray-800">{category.name}</h3>
+          <div key={index} className="text-center relative w-[347px] h-[352px] ">
+            <img src={category.image} alt={category.name} className="object-cover w-full h-full" />
+            <div className="absolute bottom-0 left-0 w-full h-24 bg-pink-300 opacity-35 blur-sm"></div>
+            <div className="absolute bottom-4 left-5">
+              <h3 className="mt-4 text-lg text-white">{category.name}</h3>
+            </div>
           </div>
         ))}
       </div>
